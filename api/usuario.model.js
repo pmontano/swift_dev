6eb80326cea7daab0e3e7ac07dc416254ab82efa
Orlando <otrejosm@ucenfotec.ac.cr>
2019-07-14 00:12:56 -0600

    Agregando direccion 2

--- api/usuario.model.js
+++ api/usuario.model.js
@@ -6,8 +6,9 @@ let userSchema = new mongoose.Schema({
     correo: { type: String, required: true },
     contrasena: { type: String, required: true },
     nombre: { type: String, required: true },
-    direccion : {type: String, required: true}
-    segundo_apellido: { type: String, required: true }
+    direccion : {type: String, required: true},
+    segundo_apellido: { type: String, required: true },
+    direccion2: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Usuario', userSchema, "usuarios");
