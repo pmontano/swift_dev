04d4e14a1f3e3044aab350b6b7bb29b73840b201
Orlando <otrejosm@ucenfotec.ac.cr>
2019-07-30 13:54:32 -0600

    mn

--- api/usuario.model.js
+++ api/usuario.model.js
@@ -4,12 +4,7 @@ let mongoose = require('mongoose');
 
 let userSchema = new mongoose.Schema({
     correo: { type: String, required: true },
-    contrasena: { type: String, required: true },
-    nombre22q: { type: String, required: true },
-    direccion : {type: String, required: true}
-    segundo_apellido: { type: String, required: true },
-    direccion2: {type: String, required: true}
-    nombre: { type: String, required: true },
+    contrasena: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Usuario', userSchema, "usuarios");
